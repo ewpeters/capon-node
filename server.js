3c9f03ac0fdff95e1b39191b950cf9929f528482
Erik Petersen <erik@telmate.com>
2012-02-23 20:03:54 -0800

    set up server.js to use dev database and hand of incoming texts to the user controller

--- server.js
+++ server.js
@@ -1,15 +1,54 @@
+var http = require("http");
+var querystring = require('querystring');
 process.addListener('uncaughtException', function (err, stack) {
   console.log('------------------------');
   console.log('Exception: ' + err);
   console.log(err.stack);
   console.log('------------------------');
 });
-
+var UserController = require('./lib/user_controller');
 var Schema = require('./lib/schema');
 
-var testSchema = new Schema({
-  database: "capon_test",
+var mySchema = new Schema({
+  database: "capon_development",
   user: "capon",
   host: "localhost"
 });
+var userController = new UserController(mySchema, {});
 
+function onRequest(request, response) {
+  response.writeHead(200, {"Content-Type": "text/html"});
+  // response.write("<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n");
+  var data = '';
+  request.on('data', function (chunk) {
+    data += chunk;
+  });
+  
+  request.on('end', function() {
+    var dataHash = querystring.parse(data);
+    
+    userController.handleMessage(dataHash.From, dataHash.Body, 1, function(message) {
+      response.write( "<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n");
+      response.write("<Response>");
+      
+      response.write("<Sms>")
+      for (var i in message) {
+        var line = message[i];
+        response.write(line);
+        response.write("\n");
+      }
+      response.write("</Sms>");
+      response.write("</Response>")
+      response.end();
+    })
+
+    
+  })
+  
+  // <Response>
+  //     <Sms>Hello, Mobile Monkey</Sms>
+  // </Response>
+  
+}
+
+http.createServer(onRequest).listen(80);
